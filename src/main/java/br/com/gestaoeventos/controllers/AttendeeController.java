0e257a63329c5305911c34f3b4e20f284bfef8c0
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-05 14:06:49 -0300

    feat: estrutura controllers

---
+++ src/main/java/br/com/gestaoeventos/controllers/AttendeeController.java
@@ -0,0 +1,9 @@
+package br.com.gestaoeventos.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+}
---
+++ src/main/java/br/com/gestaoeventos/controllers/EventController.java
@@ -0,0 +1,9 @@
+package br.com.gestaoeventos.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+}
