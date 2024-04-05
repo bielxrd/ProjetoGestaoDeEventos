70a1c716d343f299e1c9c47bf8f97bbb3bf4d092
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-05 13:11:55 -0300

    feat: model do checkin com fk do participante

---
+++ src/main/java/br/com/gestaoeventos/domain/checkin/CheckIn.java
@@ -0,0 +1,31 @@
+package br.com.gestaoeventos.domain.checkin;
+
+import br.com.gestaoeventos.domain.attendee.Attendee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+
+}
