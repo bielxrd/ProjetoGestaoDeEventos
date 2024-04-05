e44654e3c895307bcad5b783ea64142a2c974da4
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-05 12:25:53 -0300

    estrutura padrao

---
+++ src/main/java/br/com/gestaoeventos/PassinApirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaoeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassinApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassinApirestApplication.class, args);
+	}
+
+}
