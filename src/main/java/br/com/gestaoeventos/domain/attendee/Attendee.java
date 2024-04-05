ec764eca9cede044cc4d9c4b2b1c30b1e43dfdcd
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-05 13:11:36 -0300

    feat: model do participante com fk do evento

---
+++ src/main/java/br/com/gestaoeventos/domain/attendee/Attendee.java
@@ -0,0 +1,36 @@
+package br.com.gestaoeventos.domain.attendee;
+
+import br.com.gestaoeventos.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Attendee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id" , nullable = false)
+    private Event event;
+
+    @Column(nullable = false, name = "created_at")
+    public LocalDateTime createdAt;
+}
