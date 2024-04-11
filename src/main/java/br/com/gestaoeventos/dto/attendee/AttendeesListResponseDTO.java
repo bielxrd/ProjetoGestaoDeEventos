c75740a1522d45c4c36bdc1ccdf3c6bcfc243db3
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-11 01:47:37 -0300

    feat: DTO para lista de participantes

--- src/main/java/br/com/gestaoeventos/dto/attendee/AttendeesListResponseDTO.java
+++ src/main/java/br/com/gestaoeventos/dto/attendee/AttendeesListResponseDTO.java
@@ -1,10 +1,12 @@
 package br.com.gestaoeventos.dto.attendee;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class AttendeesListResponseDTO {
     List<AttendeeDetails> attendees;
 }
