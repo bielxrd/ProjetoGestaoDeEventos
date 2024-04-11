40e8b829c5a718daba6692f06615b5fae3344221
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-11 01:45:46 -0300

    feat: metodo para buscar participante pelo id para verificar se o checkin foi feito

--- src/main/java/br/com/gestaoeventos/repositories/CheckinRepository.java
+++ src/main/java/br/com/gestaoeventos/repositories/CheckinRepository.java
@@ -3,5 +3,9 @@ package br.com.gestaoeventos.repositories;
 import br.com.gestaoeventos.domain.checkin.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
