488f51e03dc33ddb0dfa60f9dd3f71b98cb78313
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-11 01:46:55 -0300

    feat: metodo para pegar todos os participantes por um id do evento, adiciona em uma lista todos os participante e retorna DTO para exibir os detalhes dos attendees

--- src/main/java/br/com/gestaoeventos/services/AttendeeService.java
+++ src/main/java/br/com/gestaoeventos/services/AttendeeService.java
@@ -1,13 +1,18 @@
 package br.com.gestaoeventos.services;
 
 import br.com.gestaoeventos.domain.attendee.Attendee;
+import br.com.gestaoeventos.domain.checkin.CheckIn;
+import br.com.gestaoeventos.dto.attendee.AttendeeDetails;
 import br.com.gestaoeventos.dto.attendee.AttendeesListResponseDTO;
 import br.com.gestaoeventos.repositories.AttendeeRepository;
+import br.com.gestaoeventos.repositories.CheckinRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +21,24 @@ public class AttendeeService {
     @Autowired
     private AttendeeRepository attendeeRepository;
 
+    @Autowired
+    private CheckinRepository checkinRepository;
+
     public List<Attendee> getAllAttendeesFromEvent(String eventId) {
         return this.attendeeRepository.findByEventId(eventId);
     }
 
     public AttendeesListResponseDTO getEventsAttendee(String eventId){
         List<Attendee> attendeeList = getAllAttendeesFromEvent(eventId);
+
+       List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
+           Optional<CheckIn> checkIn = this.checkinRepository.findByAttendeeId(attendee.getId());
+
+           LocalDateTime checkedInAt = checkIn.isPresent() ? checkIn.get().getCreatedAt() : null;
+           return new AttendeeDetails(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkedInAt);
+       }).toList();
+
+        return new AttendeesListResponseDTO(attendeeDetailsList);
     }
 
 }
