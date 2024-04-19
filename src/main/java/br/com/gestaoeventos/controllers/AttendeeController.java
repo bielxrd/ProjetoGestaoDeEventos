d40baf46daf543bcf156a6fd45532097a763c0c1
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-19 01:42:43 -0300

    feat: metodo para trazer badge do participante

--- src/main/java/br/com/gestaoeventos/controllers/AttendeeController.java
+++ src/main/java/br/com/gestaoeventos/controllers/AttendeeController.java
@@ -1,9 +1,27 @@
 package br.com.gestaoeventos.controllers;
 
+import br.com.gestaoeventos.dto.attendee.AttendeeBadgeResponseDTO;
+import br.com.gestaoeventos.services.AttendeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
+
+    @Autowired
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO attendeeBadge = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(attendeeBadge);
+    }
 }
