c65670f33fad34f7d9082c3afd70b91bd23afd94
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-19 01:43:41 -0300

    feat: metodo para buscar attendee pelo id do event e email

--- src/main/java/br/com/gestaoeventos/repositories/AttendeeRepository.java
+++ src/main/java/br/com/gestaoeventos/repositories/AttendeeRepository.java
@@ -4,7 +4,10 @@ import br.com.gestaoeventos.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
     List<Attendee> findByEventId(String id);
+
+    Optional<Attendee> findByEventIdAndEmail(String email, String eventId);
 }
