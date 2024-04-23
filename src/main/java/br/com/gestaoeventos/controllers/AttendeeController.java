9539b07871143b360dae7f2885c06d7e18f939b7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-23 17:57:12 -0300

    feat: rota para registrar checkin

--- src/main/java/br/com/gestaoeventos/controllers/AttendeeController.java
+++ src/main/java/br/com/gestaoeventos/controllers/AttendeeController.java
@@ -2,14 +2,14 @@ package br.com.gestaoeventos.controllers;
 
 import br.com.gestaoeventos.dto.attendee.AttendeeBadgeResponseDTO;
 import br.com.gestaoeventos.services.AttendeeService;
+import br.com.gestaoeventos.services.CheckInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/attendees")
@@ -19,9 +19,20 @@ public class AttendeeController {
     @Autowired
     private final AttendeeService attendeeService;
 
+
+
     @GetMapping("/{attendeeId}/badge")
     public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
         AttendeeBadgeResponseDTO attendeeBadge = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
         return ResponseEntity.ok(attendeeBadge);
+    }
+
+    @PostMapping("/{attendeeId}/check-in")
+    public ResponseEntity doCheckIn(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        this.attendeeService.checkInAttendee(attendeeId);
+
+        URI uri = uriComponentsBuilder.path("/atendees/{attendeeId}/badge").buildAndExpand(attendeeId).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
