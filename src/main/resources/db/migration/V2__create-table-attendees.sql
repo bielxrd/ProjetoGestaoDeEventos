c70aab59827b096f3a27923392ea74cbbb94247f
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-15 02:29:03 -0300

    muda regra para permitir attendee sem eventp

--- src/main/resources/db/migration/V2__create-table-attendees.sql
+++ src/main/resources/db/migration/V2__create-table-attendees.sql
@@ -2,7 +2,8 @@ CREATE TABLE attendees (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    event_id VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) not null,
+    event_id VARCHAR(255) NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT attendees_event_id_fkey FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
